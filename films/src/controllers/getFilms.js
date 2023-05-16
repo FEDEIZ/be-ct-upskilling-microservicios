5655bab89abe301ece2275112b321f001a365810
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-05-16 12:55:35 -0300

    hot fix

--- films/src/controllers/getFilms.js
+++ films/src/controllers/getFilms.js
@@ -1,10 +1,5 @@
-const { Router } = require("express");
 const repository = require("../data");
 
-const filmsRouter = Router();
-
-filmsRouter.get("/", async (req, res) => {
+module.exports = async (req, res) => {
   return res.status(200).json(await repository.list());
-});
-
-module.exports = filmsRouter;
+};
--- planets/src/controllers/getPlanets.js
+++ planets/src/controllers/getPlanets.js
@@ -1,10 +1,5 @@
-const { Router } = require("express");
 const repository = require("../data");
 
-const planetsRouter = Router();
-
-planetsRouter.get("/", async (req, res) => {
+module.exports = async (req, res) => {
   return res.status(200).json(await repository.list());
-});
-
-module.exports = planetsRouter;
+};

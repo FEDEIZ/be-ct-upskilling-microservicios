0992b49847ea2247a6f24e45e3d77dad029c1acf
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-05-16 22:16:53 -0300

    microservices in docker containers

--- films/src/controllers/createFilm.js
+++ films/src/controllers/createFilm.js
@@ -1,5 +1,5 @@
 const repository = require("../data");
-const { response } = require("../../../utils");
+const { response } = require("../utils");
 
 module.exports = async (req, res) => {
   return response(res, 200, await repository.create());
